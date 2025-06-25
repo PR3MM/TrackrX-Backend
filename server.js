e6d98b8b89710ca744d389cebc828722bd6e8329
Premved Dhote <premveddhote@gmail.com>
2025-06-25 21:36:20 +0530

    Add error handling middleware for 404 and 500 responses

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ connectDB();
 
 
 app.use('/', router);
+// Handle 404 errors
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+// Handle errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Internal Server Error' });
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on website at http://localhost:${port}`);
